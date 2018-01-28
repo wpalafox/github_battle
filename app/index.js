7c6bbc36295b926e1382485ebcc8495785a62f2b
Will Palafox <wpalafox@gmail.com>
2018-01-28 09:52:26 -0600

    Sunday Morning Practice

--- app/index.js
+++ app/index.js
@@ -2,12 +2,9 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 require('./index.css');
 
-// state
-// lifescycle event 
-// UI 
 
 
-
+/*
 class App extends Component {
 	render() {
 		return(
@@ -17,8 +14,50 @@ class App extends Component {
 		)
 	}
 }
+*/
+
+var USER_DATA = {
+	name: 'Will Palafox',
+	img: 'http://i0.kym-cdn.com/entries/icons/mobile/000/025/249/fiona.jpg',
+	username:  'wpalafox'
+}
 
 
 
-ReactDOM.render(<App />, document.getElementById('app')
+
+class Badge extends React.Component {
+	render(){
+	 return(
+	  <div>
+	  	<img src={this.props.user.img} 
+	  	alt='Avatar'
+	  
+		/>
+
+	  	<h1>Name:{this.props.user.name} </h1>
+	  	<h3>username:{this.props.user.username} </h3>
+	  </div>				
+	  )
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+ReactDOM.render(
+	<Badge user={USER_DATA}/>,
+	document.getElementById('app')
 );
+
+
+
+
+
+
