134ef517d2e9a46bc89416448eb8e29390b64750
Will Palafox <wpalafox@gmail.com>
2018-02-18 09:54:10 -0600

    understanding the this keyword

--- app/popular.js
+++ app/popular.js
@@ -5,39 +5,48 @@ class Popular extends React.Component {
   
 
 
-/*
-  constructor() {
-  	super(props);
+
+constructor(props) {
+  super(props);
   	this.state = {
-		selectedLanguage: 'All'
-	};
+			selectedLanguage: 'All'
+		};
+
+
+	this.updateLanguage = this.updateLanguage.bind(this);
+
 }	
 
 updateLanguage(lang){
 	this.setState(function(){
-		return(
+		//console.log(lang)
+		return{
 			selectedLanguage: lang
+		}
 
-			)
-
-
-
+		
 	});
 }
-*/
+
+
 render () {
 		
 		var languages = ['All', 'Javascript', 'Ruby', 'Java', 'CSS', 'Python'];
-		
+
 		return(
 			<ul className='languages'>
+			<p>Selected Language: {this.state.selectedLanguage} </p>
+				
 				{languages.map(function(lang) {
 					return (
-					  <li key={lang}>
+					  <li 
+					  	style={lang === this.state.selectedLanguage ? {color: '#d0021b'}: null}
+					  	onClick={this.updateLanguage.bind(null, lang)}
+					  	key={lang}>
 					  	{lang}	
 					  </li>
 						)
-				})}
+				}, this)}
 			</ul>
 		
 		)
